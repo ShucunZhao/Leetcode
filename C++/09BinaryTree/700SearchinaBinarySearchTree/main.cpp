32c9c86ef6e87bf0bb8156d0e252d7a27fd4acf8
ShucunZhao <frankabe001@gmail.com>
2022-10-27 20:32:25 +0800

    submitted 700: use feature of binary search tree.

---
+++ C++/09BinaryTree/700SearchinaBinarySearchTree/main.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL) return NULL;
+        if(root->val==val) return root;
+        TreeNode * cur = NULL;
+        if(root->val > val) cur=searchBST(root->left, val);
+        if(root->val < val) cur=searchBST(root->right, val);
+        return cur;
+    }
+};
