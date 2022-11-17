c02bd4f52fccce27240d2f16ab59facf2e7f5823
ShucunZhao <frankabe001@gmail.com>
2022-11-17 15:38:40 +0800

    submitted 279: Same as 322.

---
+++ C++/12DynamicProgramming/279PerfectSquares/sameAs322.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n+1, INT_MAX);
+        dp[0] = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j*j<=i;j++){
+                dp[i] = min(dp[i], dp[i-j*j]+1);
+            }
+        }
+        // for(int i=0;i<=n;i++){
+        //     cout<<"dp["<<i<<"]:"<<dp[i]<<endl;
+        // }
+        return dp[n];
+    }
+};
