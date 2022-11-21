2e87e62c27dfaeebbdea3da34c2f9b9edacd4b76
ShucunZhao <frankabe001@gmail.com>
2022-11-21 13:54:30 +0800

    submitted 1143: similar to 718, also need to draw the x,y picture to realize.

--- C++/12DynamicProgramming/1143LongestCommonSubsequence/solwithDP.cpp
+++ C++/12DynamicProgramming/1143LongestCommonSubsequence/solwithDP.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         vector<vector<int> > dp(text1.size()+1, vector<int>(text2.size()+1, 0));
-        int ans = 0;
+        // int ans = 0;
         for(int i=1;i<=text1.size();i++){
             for(int j=1;j<=text2.size();j++){
                 if(text1[i-1]==text2[j-1]){
@@ -11,9 +11,10 @@ public:
                 else{
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
-                ans = max(ans, dp[i][j]);
+                // ans = max(ans, dp[i][j]);//Don't need this step because the final element will be the maximum.
             }
         }
-        return ans;
+        // return ans;
+        return dp[text1.size()][text2.size()];
     }
 };
