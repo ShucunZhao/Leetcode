8decfc9591dc828b232a28f065fa7e3eef027281
ShucunZhao <frankabe001@gmail.com>
2022-11-23 16:54:53 +0800

    submitted 35.

---
+++ C++/01BinarySearch/35SearchInsertPosition/solwithBS.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l=0,r=nums.size();
+        int m;
+        while(l<r){
+            m = l+(r-l)/2;
+            if(nums[m]>target){
+                r = m;
+            }
+            else if(nums[m]<target){
+                l = m+1;
+            }
+            else{
+                return m;
+            }
+        }
+        return r;
+    }
+};
