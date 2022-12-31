66023c531e32a09b508bb1092b226f0711056bfc
ShucunZhao <frankabe001@gmail.com>
2022-12-31 20:21:00 +0800

    updated 322: add new sol.

--- C++/12DynamicProgramming/322CoinChange/newSol.cpp
+++ C++/12DynamicProgramming/322CoinChange/newSol.cpp
@@ -5,9 +5,9 @@ public:
         dp[0] = 0;
         for(int i=1;i<=amount;i++){
             for(int j=0;j<coins.size();j++){
-                if(coins[j]<=i&&dp[i-coins[j]]!=-1){
-                    if(dp[i]==-1) dp[i] = dp[i-coins[j]]+1;
-                    else dp[i] = min(dp[i], dp[i-coins[j]]+1);
+                if(coins[j]<=i&&dp[i-coins[j]]!=-1){//If i larger than conins[j], judge if coins[j] can choose (which means "dp[i-coins[j]]" should be calculated, dp[i-coins[j]] shouldn't be -1). 
+                    if(dp[i]==-1) dp[i] = dp[i-coins[j]]+1;//If dp[i] has not been calculate let it be dp[i-coins[j]]+1
+                    else dp[i] = min(dp[i], dp[i-coins[j]]+1);//Else choose the smaller one when try 0-coins.size of coins.
                 }
             }
         }
