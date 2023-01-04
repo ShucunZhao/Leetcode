d49db233e2d395389766b34f84485f685154f653
ShucunZhao <frankabe001@gmail.com>
2023-01-04 15:33:31 +0800

    updated 115: updated some detail of expanation of DP sol.

--- C++/12DynamicProgramming/115DistinctSubsequences/solwithDP.cpp
+++ C++/12DynamicProgramming/115DistinctSubsequences/solwithDP.cpp
@@ -1,8 +1,9 @@
 /*
  * dp[i][j]: The number of cases subsequence that end with index i in original string can generate 
              the subsequence that end with index j in target string.
- * Infer formula: if s[i-1] == s[j-1] will have two cases:
-                      I.Use s[i-1]: dp[i][j] should be dp[i-1][j-1] 
+ * Infer formula: if s[i-1] == t[j-1] will have two cases:
+                      I.Use s[i-1]: dp[i][j] should be dp[i-1][j-1], because if s[i-1] is equal to t[i-1] 
+						that means adding these two characters will not affect the result, which is also dp[i-1][j-1].
                       II.Don't use s[i-1] and jump into next s[i]: dp[i][j] should be dp[i-1][j]
                       then: dp[i][j] = dp[i-1][j-1] + dp[i-1][j].
                   else: because it is not equal then you must jump current s[i-1] to s[i]:
