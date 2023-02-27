8521948b9fdf76187da371bc4f1202aef4b4e3ed
ShucunZhao <frankabe001@gmail.com>
2023-02-27 15:02:52 +0800

    submitted 235: sol with BST feature by judging the left and right descendant first.

---
+++ C++/09BinaryTree/235LowestCommonAncestorOfaBinarySearchTree/solwithBST(new).cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode * traverse(TreeNode* root, TreeNode* l, TreeNode* r){
+        if(!root) return NULL;
+        if(root->val<l->val){
+            return lowestCommonAncestor(root->right,l,r);
+        }
+        else if(root->val>r->val){
+            return lowestCommonAncestor(root->left, l, r);
+        }
+        return root;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(p->val>q->val){
+            return traverse(root, q, p);
+        }
+        return traverse(root, p, q);
+    }
+};
