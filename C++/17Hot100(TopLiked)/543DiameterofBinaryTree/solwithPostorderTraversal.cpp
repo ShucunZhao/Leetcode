b525e71e3c5cf78ef00d42e068c8bc4866d0338e
ShucunZhao <frankabe001@gmail.com>
2023-03-23 11:47:28 +0800

    submitted 543: sol with postorder traversal.

---
+++ C++/17Hot100(TopLiked)/543DiameterofBinaryTree/solwithPostorderTraversal.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = 0;
+    int postTraverse(TreeNode * root){
+        if(!root) return 0;
+        int L = postTraverse(root->left);
+        int R = postTraverse(root->right);
+        ans = max(ans, L+R);
+        return max(L,R)+1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        postTraverse(root);
+        return ans;
+    }
+};
