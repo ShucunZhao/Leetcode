a2460315ac25bdfb7e409da6158f59cfe647506b
ShucunZhao <frankabe001@gmail.com>
2023-05-17 13:34:53 +0800

    submitted 64: sol with arithmetic sequence sum formula.

---
+++ C++/19JZOffer/64CalculateTheSumFrom1ToN/solwithArithmeticSequenceSumFormula.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int Sum_Solution(int n) {
+        /*
+            Sol with the formula of the sum of Arithmetic Progression first n items:
+            Sn = ((a1+an)*n)/2
+            and an = a1 + (n-1)*d where d is 1 so that an = a1 + (n-1)
+            so:
+                Sn = ((a1+a1+(n-1))*n)/2
+                   = ((2a1+n-1))*n/2
+                   where a1 = 1
+                   so:
+                    Sn = ((2+n-1)*n)/2
+                       = ((n+1)*n)/2
+                       = (n^2+n)/2
+                       = (n^2+n)>>2
+        */   
+        return (pow(n,2.0)+n)/2;
+    }
+};
