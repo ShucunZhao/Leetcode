7664b14ae1fd6e52a6c2890225396ef3a55025b4
ShucunZhao <frankabe001@gmail.com>
2023-06-24 15:05:01 +0800

    updated 78: sol with backtracking.

---
+++ C++/10Backtracking/78Subsets/solwithBacktracking.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> path;
+    vector<vector<int> > ans;
+    void backtracking(vector<int> & nums, int start){
+        if(start==nums.size()){
+            return;
+        }
+        for(int i=start;i<nums.size();i++){
+            path.push_back(nums[i]);
+            ans.push_back(path);
+            backtracking(nums, i+1);
+            path.pop_back();
+        }
+        return;
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        /*
+            
+            [1,2,3]
+        */
+        ans.push_back({});
+        backtracking(nums, 0);
+        return ans;
+    }
+};
