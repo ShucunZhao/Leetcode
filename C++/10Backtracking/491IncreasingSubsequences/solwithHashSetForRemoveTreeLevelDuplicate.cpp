6a5e048ddd84781e4886c587d9ec207f1467d013
ShucunZhao <frankabe001@gmail.com>
2023-06-25 11:41:47 +0800

    submitted 491: sol with hash set to remove tree level(for) duplicates.

---
+++ C++/10Backtracking/491IncreasingSubsequences/solwithHashSetForRemoveTreeLevelDuplicate.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> path;
+    vector<vector<int> > ans;
+    void backtracking(vector<int> & nums, vector<bool> & used, int start){
+        if(start==nums.size()){
+            return;
+        }
+        unordered_set<int> mset;
+        for(int i=start;i<nums.size();i++){
+            if(mset.count(nums[i])) continue;
+            if(i>0&&nums[i-1]==nums[i]&&!used[i-1]){
+                continue;
+            }
+            if(path.empty()||path.back()<=nums[i]){
+                mset.insert(nums[i]);
+                path.push_back(nums[i]);
+                used[i] = true;
+                if(path.size()>1){
+                    ans.push_back(path);
+                }
+                backtracking(nums, used, i+1);
+                path.pop_back();
+                used[i] = false;
+            } 
+        }
+        return;
+    }
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        vector<bool> used(nums.size(), false);
+        backtracking(nums, used, 0);
+        return ans;
+    }
+};
