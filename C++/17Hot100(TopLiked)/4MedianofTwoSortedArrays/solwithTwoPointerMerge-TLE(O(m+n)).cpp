10b51b224f5edc1ea8060dbdd9f8df68eaa41e6e
ShucunZhao <frankabe001@gmail.com>
2023-06-28 14:13:51 +0800

    updated 4: sol with merge sort.

--- C++/17Hot100(TopLiked)/4MedianofTwoSortedArrays/solwithTwoPointerMerge-TLE(O(m+n)).cpp
+++ C++/17Hot100(TopLiked)/4MedianofTwoSortedArrays/solwithTwoPointerMerge-TLE(O(m+n)).cpp
@@ -15,7 +15,7 @@ public:
         int size = nums1.size()+nums2.size();
         vector<int> nums3(size);
         int i = 0, l = 0, r = 0;
-        for(int l,r;l<nums1.size()||r<nums2.size();){
+        for(l,r;l<nums1.size()&&r<nums2.size();){
             // if()
             if(nums1[l]<nums2[r]){
                 nums3[i++] = nums1[l++];
@@ -24,7 +24,7 @@ public:
                 nums3[i++] = nums2[r++];
             }
             // nums3[i++]=(nums1[l]<nums2[r])?nums1[l++]:nums2[r++];
-            cout<<nums3[i-1]<<endl;
+            //cout<<nums3[i-1]<<endl;
         }
         while(l<nums1.size()){
             nums3[i++] = nums1[l++];
@@ -32,8 +32,12 @@ public:
         while(r<nums2.size()){
             nums3[i++] = nums2[r++];
         }
-        for(int i : nums3) cout<<i<<",";
-        cout<<endl;
-        return -1;
+        // for(int i : nums3) cout<<i<<",";
+        // cout<<endl;
+        if(size&1){
+            return nums3[(size>>1)];
+        }
+        return ((double)nums3[(size>>1)-1]+(double)nums3[(size>>1)])/2;
+        // return -1;
     }
 };
