8d6acaf29a4c006fa35eba73985e40cd7cbd587a
ShucunZhao <frankabe001@gmail.com>
2023-07-12 20:34:57 +0800

    submitted 241: sol with divide and conquer by left and right pointers.

---
+++ C++/20Company/13Microsoft_CN/241DifferentWaystoAddParentheses/UpgradedVersionOfsol1ByMemorization.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    unordered_map<string, vector<int> > ms;
+    vector<int> diffWaysToCompute(string expression) {
+        if(ms.count(expression)){
+            return ms[expression];
+        }
+        vector<int> res;
+        // int i = 0;
+        // while(i)
+        // if(expression.size()==1){
+        //     res.push_back(expression[0]-'0');
+        //     return res;
+        // }
+        for(int i=0;i<expression.size();i++){
+            char c = expression[i];
+            if(c=='-'||c=='+'||c=='*'){
+                // cout<<expression.substr(0,i)<<endl;
+                // cout<<expression.substr(i+1)<<endl;
+                vector<int> left = diffWaysToCompute(expression.substr(0,i));
+                vector<int> right = diffWaysToCompute(expression.substr(i+1));
+                for(int l=0;l<left.size();l++){
+                    for(int r=0;r<right.size();r++){
+                        if(c=='-'){
+                            res.push_back(left[l]-right[r]);
+                        }
+                        else if(c=='+'){
+                            res.push_back(left[l]+right[r]);
+                        }
+                        else{
+                            res.push_back(left[l]*right[r]);
+                        }
+                        
+                    }
+                }
+            }
+        }
+        if(res.empty()){
+            res.push_back(stoi(expression));
+        }
+        ms[expression] = res;
+        return res;
+    }
+};
