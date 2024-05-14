3023471839954d5a3c75ed24c540a905fa59b283
ShucunZhao <frankabe001@gmail.com>
2024-05-14 10:20:12 +0800

    submitted jz20: sol with mocking

--- C++/acmMode/19JZOffer/jz20ValueStringExpression/main.cpp
+++ C++/acmMode/19JZOffer/jz20ValueStringExpression/main.cpp
@@ -9,34 +9,55 @@ using namespace std;
 class Solution {
 public:
     bool checkInt(string str) {
-        bool isNumber = false;
-        for (int i = 0; i < str.size();++i) {
-            if (isalpha(str[i])) return false;
-            if (str[i] == '+' || i != 0) return false;
-            if (str[i] == '+' || i != 0) return false;
-            if (isdigit(str[i])) isNumber = true;
+        if (str[0] != '+' && str[0] != '-' && !isdigit(str[0])) return false;
+        if (str.size() < 2 && !isdigit(str[0])) return false;
+        for (int i = 1; i < str.size();++i) {
+            if (!isdigit(str[i])) return false;
         }
-        return isNumber;
+        return true;
+    }
+    bool checkNumber(string str) {
+        for (char& c : str) {
+            if (!isdigit(c)) return false;
+        }
+        return true;
+    }
+    bool checkDecimal(string str) {
+        if (str[0] != '+' && str[0] != '-' && !isdigit(str[0])&&!str[0]=='.') return false;
+        size_t pos = str.find(".");
+        string first = str.substr(0, pos);
+        string second = str.substr(pos + 1);
+        if (first.empty()&&!second.empty()) {
+            return checkNumber(second);
+        }
+        else if (!first.empty() && second.empty()) {
+            return checkNumber(first);
+        }
+        else if (!first.empty() && !second.empty()) {
+            if (first == "+" || first == "-") return checkNumber(second);
+            return checkNumber(first)&& checkNumber(second);
+        }
+        return false;
     }
     bool isNumeric(string str) {
         if (str.empty()) return false;
         // write code here
         size_t l = str.find_first_not_of(" ");
         size_t r = str.find_last_not_of(" ");
+        if (1 == string::npos || r == string::npos) {
+            return false;
+        }
         str = str.substr(l, r - l + 1);
         size_t pos;
         if ((pos = str.find("e")) != string::npos || (pos = str.find("E")) != string::npos) {
             string first = str.substr(0, pos);
             string second = str.substr(pos + 1);
-            if ((pos = first.find(".")) != string::npos || (pos = second.find(".")) != string::npos) return false;
-            return checkInt(first) && checkInt(second);
+            if (!checkInt(first) && !checkDecimal(first)) return false;
+            if (!checkInt(second)) return false;
+            return true;
         }
         else if((pos = str.find(".")) != string::npos){
-            if (str[0] == '.') return checkInt(str.substr(1));
-            if (str[str.size() - 1] == ',') return checkInt(str.substr(0, pos));
-            string first = str.substr(0, pos);
-            string second = str.substr(pos + 1);
-            return checkInt(first) && checkInt(second);
+            return checkDecimal(str);
         }
         return checkInt(str);
     }
