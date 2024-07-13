3971233446aae3e86afa6bf372c938843166ca31
ShucunZhao <frankabe001@gmail.com>
2024-07-13 09:47:37 +0800

    submitted lc912: sol with bubble, merge and quick sort.

--- C++/acmMode/17Hot100(TopLiked)/lc912SortAnArray/main.cpp
+++ C++/acmMode/17Hot100(TopLiked)/lc912SortAnArray/main.cpp
@@ -8,8 +8,73 @@ using namespace std;
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        bubbleSort(nums);
+        // bubbleSort(nums);
+        quickSort(nums);
         return nums;
+    }
+
+    void quickSort(vector<int>& nums) {
+        /*
+                   l
+            [0,0,1,1,2,5]
+                   r
+             p
+             pivot: 1
+        */
+        quick(nums, 0, nums.size() - 1);
+        return;
+    }
+    void quick(vector<int>& nums, int l, int r) {
+        if (l >= r) return;
+        int mid = pivot(nums, l, r);
+        quick(nums, l, mid-1);
+        quick(nums, mid + 1, r);
+        return;
+    }
+    int pivot(vector<int>& nums, int l, int r) {
+        if (l >= r) return l;
+        int p = nums[l];
+        while (l < r) {
+            while (l<r && nums[r]>p) r--;
+            if (l == r) break;
+            nums[l++] = nums[r];
+            while (l < r && nums[l] <= p) l++;
+            if (l == r) break;
+            nums[r--] = nums[l];
+        }
+        nums[l] = p;
+        return l;
+    }
+
+
+    void mergeSort(vector<int>& nums) {
+        vector<int> tmp(nums.size());
+        divide(nums,tmp, 0, nums.size() - 1);
+    }
+
+    void divide(vector<int>& nums, vector<int>& tmp, int l, int r) {
+        if (l >= r) return;
+        int mid = l + (r - l) / 2;
+        divide(nums, tmp, l, mid);
+        divide(nums, tmp, mid + 1, r);
+        merge(nums, tmp, l, mid, r);
+    }
+
+    void merge(vector<int>& nums, vector<int>& tmp, int l, int mid, int r) {
+        int x = l;
+        int left = l, right = mid+1;
+        while (left <= mid && right <= r) {
+            if (nums[left] < nums[right]) {
+                tmp[x++] = nums[left++];
+            }
+            else {
+                tmp[x++] = nums[right++];
+            }
+        }
+        while (left <= mid) tmp[x++] = nums[left++];
+        while (right <= r) tmp[x++] = nums[right++];
+        for (int i = l; i <= r; ++i) nums[i] = tmp[i];
+        return;
     }
 
     void bubbleSort(vector<int>& nums) {
